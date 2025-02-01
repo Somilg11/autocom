a0e3ff8e91524a110d28538b2ceb5da21c74ca95
Abhijeet Mishra <abhijeetmishra@Abhijeets-Mac.local>
2025-02-01 17:11:44 +0530

    initialised prisma, auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import {
-  ClerkProvider,
+  ClerkProvider
 } from '@clerk/nextjs'
 import { ThemeProvider } from "@/components/theme-provider";
 
