5763125a663f48ac3a282b992bed82c7af7029dd
Abhijeet Mishra <abhijeetmishra@Abhijeets-Mac.local>
2025-02-02 23:56:36 +0530

    resolve conflict

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,18 +1,15 @@
-
-import Sidebar from "@/components/global/sidebar";
+import { onBoardUser } from "@/actions/user";
+import { redirect } from "next/navigation";
 import React from "react";
 
-type Props = {
-    children: React.ReactNode
-    params: {slug: string}
-}
+type Props = {}
 
-const Page = ({children, params}: Props) => {
-    return (
-        <div>
-            <Sidebar slug={params.slug}></Sidebar>
-        </div>
-    )
+const Page = async (props: Props) => {
+    const user = await onBoardUser();
+    if(user.status === 200 || user.status === 201){
+        return redirect(`/dashboard/${user.data.firstname}${user.data.lastname}`)
+    }
+    return redirect('/sign-in')
 }
 
 export default Page;
