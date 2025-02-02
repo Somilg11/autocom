97088927f1670dffb646c237265c9fd5b19f36ce
Somil Gupta <gsomil93@gmail.com>
2025-02-03 00:41:02 +0530

    added tanstack query, optimization, caching

--- src/actions/user/index.ts
+++ src/actions/user/index.ts
@@ -49,3 +49,14 @@ export const onBoardUser = async () => {
         return {status: 500, data: "Internal Server Error"}
     }
 }
+
+export const onUserInfo = async () => {
+    const user = await onCurrentUser()
+    try {
+        const profile = await findUser(user.id)
+        if(profile) return {status: 200, data: profile}
+        return {status: 404, data: "User not found"}
+    } catch (error) {
+        return {status: 500, data: "Internal Server Error"}
+    }
+}
--- src/app/(protected)/dashboard/[slug]/layout.tsx
+++ src/app/(protected)/dashboard/[slug]/layout.tsx
@@ -1,17 +1,22 @@
-
+import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
 import Navbar from "@/components/global/navbar";
 import Sidebar from "@/components/global/sidebar";
 import React from "react";
+import { PrefetchUserAutomations, PrefetchUserProfile } from "@/react-query/prefetch";
 
 type Props = {
     children: React.ReactNode
     params: {slug: string}
 }
 
-const Layout = ({children, params} : Props ) => {
+const Layout = async ({children, params} : Props ) => {
 
+    const query = new QueryClient()
+    await PrefetchUserProfile(query)
+    await PrefetchUserAutomations(query)
 
     return (
+        <HydrationBoundary state={dehydrate(query)}>
         <div className="p-3">
             <Sidebar slug={params.slug}></Sidebar>
             <div className="lg:ml-[250px] lg:pl-10 lg:py-5 flex flex-col overflow-auto">
@@ -19,6 +24,7 @@ const Layout = ({children, params} : Props ) => {
                 {children}
             </div>
         </div>
+        </HydrationBoundary>
     )
 }
 
